8153dfdf962fa4d13789fc18038cdfbc2add5d74
MikeNotu <mikebateman98@gmail.com>
2021-12-24 09:42:30 -0600

    Next.js Tutorial #8

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
     return (
         <div className="not-found">
             <h1>Oooops...</h1>
